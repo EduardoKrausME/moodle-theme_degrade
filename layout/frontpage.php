8e26704f04988c338b56974b5d3103c682257b04
Eduardo Kraus <kraus@eduardokraus.com>
2024-05-07 11:42:50 -0300

    Algumas raras vezes da erro porque não chamou a LIB

--- layout/frontpage.php
+++ layout/frontpage.php
@@ -25,6 +25,7 @@ defined('MOODLE_INTERNAL') || die;
 
 require_once($CFG->libdir . '/behat/lib.php');
 require_once($CFG->dirroot . '/course/lib.php');
+require_once(__DIR__ . '/../lib.php');
 
 // Add block button in editing mode.
 $addblockbutton = $OUTPUT->addblockbutton();
