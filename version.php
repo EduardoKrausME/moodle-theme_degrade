801fd34d7bf41c331a5537305e884b2de833ca1d
Eduardo Kraus <kraus@eduardokraus.com>
2025-01-13 14:12:35 -0300

    V 8.1.1

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025010600;
+$plugin->version = 2025011300;
 $plugin->release = '8.1.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022041900;
