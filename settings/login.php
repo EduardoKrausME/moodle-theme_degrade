f14262cd3ef4af1d5c170ef6b7002382159ab6d8
Eduardo Kraus <kraus@eduardokraus.com>
2026-01-05 10:40:29 -0300

    New themes logins

--- settings/login.php
+++ settings/login.php
@@ -22,9 +22,11 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use core\context\course as context_course;
+
 defined('MOODLE_INTERNAL') || die;
 
-global $CFG, $OUTPUT, $PAGE;
+global $CFG, $OUTPUT, $PAGE, $SITE;
 require_once("{$CFG->dirroot}/theme/degrade/lib.php");
 
 $page = new admin_settingpage("theme_degrade_login",
@@ -36,10 +38,27 @@ $options = [
     "selva-canopy" => get_string("logintheme_selva-canopy", "theme_degrade"),
     "clean-minimal" => get_string("logintheme_clean-minimal", "theme_degrade"),
     "clean-outline" => get_string("logintheme_clean-outline", "theme_degrade"),
+    "glassmorphism" => get_string("logintheme_glassmorphism", "theme_degrade"),
+    "serenity-med-blue" => get_string("logintheme_serenity-med-blue", "theme_degrade"),
+    "serenity-med-red" => get_string("logintheme_serenity-med-red", "theme_degrade"),
 ];
+$mustachecontext = (object)[];
+$url = $OUTPUT->get_logo_url();
+$mustachecontext->logourl = $url ? $url->out(false) : null;
+$mustachecontext->sitename = format_string($SITE->fullname);
+$mustachecontext->themes=[];
+foreach ($options as $loginthemename => $option){
+    $mustachecontext->themes[] = [
+        "login_theme" => $loginthemename,
+        "loginbackgroundimageurl" => $OUTPUT->image_url("login/{$loginthemename}", "theme_degrade")->out(false),
+    ];
+}
+$htmldescextra = $OUTPUT->render_from_template("theme_degrade/settings/login", $mustachecontext);
+$htmldescextra = preg_replace('/\s+/', ' ', $htmldescextra);
+
 $setting = new admin_setting_configselect("theme_degrade/logintheme",
     get_string("logintheme", "theme_degrade"),
-    get_string("logintheme_desc", "theme_degrade"),
+    get_string("logintheme_desc", "theme_degrade") . $htmldescextra,
     "aurora", $options);
 $page->add($setting);
 
