8e2d797e14ff2920ad469a501a6053dafbdc16b3
Eduardo Kraus <kraus@eduardokraus.com>
2026-05-06 22:08:08 -0300

    context_course

--- settings/login.php
+++ settings/login.php
@@ -22,8 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-use core\context\course as context_course;
-
 defined('MOODLE_INTERNAL') || die;
 
 global $CFG, $OUTPUT, $PAGE, $SITE;
