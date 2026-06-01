659e14eecb6e541756c12ca28977b58f04217704
Eduardo Kraus <kraus@eduardokraus.com>
2026-05-31 21:14:51 -0300

    disable moodle.Files.RequireLogin.Missing

--- accessibility-ajax.php
+++ accessibility-ajax.php
@@ -15,6 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * phpcs:disable moodle.Files.RequireLogin.Missing
  * Stores accessibility toolbar usage events.
  *
  * @package   theme_degrade
