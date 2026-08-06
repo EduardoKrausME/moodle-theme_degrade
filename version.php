708a182938d312973afd7b7dc160cd8f067cc962
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-06 08:38:56 -0300

    V 9.7.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026080400;
-$plugin->release = "9.7.0";
+$plugin->version = 2026080600;
+$plugin->release = "9.7.1";
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = "theme_degrade";
